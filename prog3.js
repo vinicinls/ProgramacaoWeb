53c2ba82bc6c5e09d5db22f2278b1696d916d690
Indio <viniciuslacerda437@gmail.com>
2025-02-20 21:07:28 -0300

    aula finalizada

--- prog3.js
+++ prog3.js
@@ -1,4 +1,12 @@
 var valor = Number(prompt("Digite um valor "));
 for(i=0;i<valor;i++){
-    document.write("Valor atual " + i + "<br>");
+   
+    // document.write("Valor atual " + i + "<br>");
+    
+   if(i % 2 == 0){
+    document.write("<p style='color:orange;'>" + i +"</p>");
+   }else{
+    document.write("<p style='color:blue;'>" + i +"</p>")
+   }
+
 }
